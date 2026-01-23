14f15281ada71263860e52b6bf7a546fb3c99c04
johnnysn <johnnysn@gmail.com>
2026-01-23 16:45:08 -0300

    docs: atualiza comentários no SecurityConfiguration.java

--- src/main/java/com/uriel/boxes/configuration/SecurityConfiguration.java
+++ src/main/java/com/uriel/boxes/configuration/SecurityConfiguration.java
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
                         .requestMatchers(HttpMethod.GET, "/health").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/signin").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/signup").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/auth/salt").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/auth/salt").permitAll() // Endpoint público para obter o salt
                         .requestMatchers(HttpMethod.POST, "/auth/token").permitAll() // refresh token
                         .anyRequest().authenticated() // Qualquer outra rota exige autenticação
                 )
